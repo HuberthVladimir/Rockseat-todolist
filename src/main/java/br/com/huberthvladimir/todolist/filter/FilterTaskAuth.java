8170706ec1ef3c0d18eb9fda9836702fe352358f
HuberthVladimir <vladimirhuberth@hotmail.com>
2023-10-11 20:49:17 -0300

    terceira aula

---
+++ src/main/java/br/com/huberthvladimir/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,58 @@
+package br.com.huberthvladimir.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.huberthvladimir.todolist.user.IUserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+   @Autowired
+   private IUserRepository userRepository;
+
+   @Override
+   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+         throws ServletException, IOException {
+
+      // Pegar dados autenticação
+      var authorization = request.getHeader("Authorization");
+
+      // conversão de senha em base64 para bytes
+      var authEncoded = authorization.substring("Basic".length()).trim();
+
+      byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+      // senha em bytes para string
+      var authString = new String(authDecode);
+
+      String[] credentials = authString.split(":");
+      String username = credentials[0];
+      String password = credentials[1];
+
+      // validação usuario
+      var user = this.userRepository.findByUsername(username);
+      if (user == null) {
+         response.sendError(401);
+      } else {
+         // validação senha
+         var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+         if (passwordVerify.verified) {
+            filterChain.doFilter(request, response);
+         } else {
+            response.sendError(401);
+         }
+      }
+
+   }
+
+}
